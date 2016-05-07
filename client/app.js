4372098642c18d556b50749c1d5015b784f34b97
JaimeVelasco <jaime@photojv.com>
2016-05-07 16:55:16 -0500

    Ready to present

--- client/app.js
+++ client/app.js
@@ -1,12 +1,14 @@
 Template.main.onCreated(function() {
-	Session.set("date", "1927");
+	// Session.set("date", "1927");
+	this.date = new ReactiveVar("1927");
 
-	// this.date = new ReactiveVar("1927");
-	this.subscribe("SanAntonioWaterLevel");
 	this.subscribe("ComalWaterLevel");
 	this.subscribe("HondoWaterLevel");
 	this.subscribe("UvaldeWaterLevel");
 	this.subscribe("SanMarcosWaterLevel");
+	this.subscribe("SanAntonioWaterLevel");
+
+
 
 
   // browser window scroll (in pixels) after which the "menu" link is shown
@@ -52,6 +54,7 @@ Template.main.onCreated(function() {
 
 
 Template.main.onRendered(function () {
+
 	// Load Map
   mapboxgl.accessToken = "pk.eyJ1Ijoiaml0b3MiLCJhIjoiOXE1SHc4VSJ9.RRtkSxtWwF2e7jBnLskfiQ";
 	let map = new mapboxgl.Map({
@@ -63,21 +66,21 @@ Template.main.onRendered(function () {
 
 Template.main.helpers({
 	date: () => {
-		return Session.get("date");
+		return Template.instance().date.get();
 	},
 
 	sanAntonio: () => {
 		let current = SanAntonioWaterLevel.find({}, {sort: {pdate: -1, limit: 1,}}).fetch();
 		if (current !== undefined) {
+			Session.set("sanAntonio", current[0].pdate);
 			return current && current[0].MaxLevel;
 		}
 	},
 
 	comal: () => {
 		let current = ComalWaterLevel.find({}, {sort: {pdate: -1, limit: 1,}}).fetch();
-		console.log("comal", current);
-
 		if (current !== undefined) {
+			// Session.set("comal", value);
 			return current && current[0].MaxLevel;
 		}
 	},
@@ -85,6 +88,7 @@ Template.main.helpers({
 	hondo: () => {
 		let current = HondoWaterLevel.find({}, {sort: {pdate: -1, limit: 1,}}).fetch();
 		if (current !== undefined) {
+			// Session.set("hondo", value);
 			return current && current[0].MaxLevel;
 		}
 	},
@@ -92,6 +96,7 @@ Template.main.helpers({
 	sanMarcos: () => {
 		let current = SanMarcosWaterLevel.find({}, {sort: {pdate: -1, limit: 1,}}).fetch();
 		if (current !== undefined) {
+			// Session.set("sanMarcos", value);
 			return current && current[0].MaxLevel;
 		}
 	},
@@ -99,8 +104,46 @@ Template.main.helpers({
 	uvalde: () => {
 		let current = UvaldeWaterLevel.find({}, {sort: {pdate: -1, limit: 1,}}).fetch();
 		if (current !== undefined) {
+			let date = current.pdate;
+			// Session.set("uvalde", date);
 			return current && current[0].MaxLevel;
 		}
+	},
+
+	sanAntonioCompare: () => {
+		let year = Session.get("sanAntonio").substring(0,4);
+		let selectedYear = Template.instance().date.get();
+		let newdate = Session.get("sanAntonio").replace(year, selectedYear);
+		Session.set("compare", newdate);
+		return Session.get("compareSA");
+	},
+
+	comalCompare: () => {
+		let year = Session.get("sanAntonio").substring(0,4);
+		let selectedYear = Template.instance().date.get();
+		let newdate = Session.get("sanAntonio").replace(year, selectedYear);
+		return Session.get("compareComal");
+	},
+
+	hondoCompare: () => {
+		let year = Session.get("sanAntonio").substring(0,4);
+		let selectedYear = Template.instance().date.get();
+		let newdate = Session.get("sanAntonio").replace(year, selectedYear);
+		return Session.get("compareHondo");
+	},
+
+	sanMarcosCompare: () => {
+		let year = Session.get("sanAntonio").substring(0,4);
+		let selectedYear = Template.instance().date.get();
+		let newdate = Session.get("sanAntonio").replace(year, selectedYear);
+		return Session.get("compareSanMarcos");
+	},
+
+	uvaldeCompare: () => {
+		let year = Session.get("sanAntonio").substring(0,4);
+		let selectedYear = Template.instance().date.get();
+		let newdate = Session.get("sanAntonio").replace(year, selectedYear);
+		return Session.get("compareUvalde");
 	}
 });
 
@@ -117,9 +160,62 @@ Template.main.events({
   },
 
 	'change input[type=range]': function(event){
-		var sliderValue = event.currentTarget.value
-		// Template.instance().date.set(sliderValue);
+		var sliderValue = event.currentTarget.value;
+		Template.instance().date.set(sliderValue);
 		Session.set("date", sliderValue);
+
+		let newdate = Session.get("compare");
+
+		Meteor.call("sanantonio", newdate, function(error, result){
+			if(error){
+				console.log("error", error);
+			}
+			if(result){
+				Session.set("compareSA", result);
+				return result;
+			}
+		});
+
+
+		Meteor.call("comal", newdate, function(error, result){
+			if(error){
+				console.log("error", error);
+			}
+			if(result){
+				Session.set("compareComal", result);
+				return result;
+			}
+		});
+
+		Meteor.call("hondo", newdate, function(error, result){
+			if(error){
+				console.log("error", error);
+			}
+			if(result){
+				Session.set("compareHondo", result);
+				return result;
+			}
+		});
+
+		Meteor.call("sanMarcos", newdate, function(error, result){
+			if(error){
+				console.log("error", error);
+			}
+			if(result){
+				Session.set("compareSanMarcos", result);
+				return result;
+			}
+		});
+
+		Meteor.call("Uvalde", newdate, function(error, result){
+			if(error){
+				console.log("error", error);
+			}
+			if(result){
+				Session.set("compareUvalde", result);
+				return result;
+			}
+		});
 
  }
 });
--- server/main.js
+++ server/main.js
@@ -25,3 +25,47 @@ Meteor.publish('SanMarcosWaterLevel', function(){
   return SanMarcosWaterLevel.find({},{sort: {pdate: -1}, limit: 1});
     // this.ready();
 });
+
+
+Meteor.methods({
+  'sanantonio':(newdate) => {
+
+    let saresult = SanAntonioWaterLevel.findOne({
+      pdate: newdate
+    });
+
+     return saresult.MaxLevel;
+  },
+
+  'comal':(newdate) => {
+
+    let Comalresult = ComalWaterLevel.findOne({
+      pdate: newdate
+    });
+     return Comalresult.MaxLevel;
+  },
+
+  'hondo':(newdate) => {
+
+    let Hondoresult = HondoWaterLevel.findOne({
+      pdate: newdate
+    });
+     return Hondoresult.MaxLevel;
+  },
+
+  'sanMarcos':(newdate) => {
+
+    let SanMarcosresult = SanMarcosWaterLevel.findOne({
+      pdate: newdate
+    });
+     return SanMarcosresult.MaxLevel;
+  },
+
+  'Uvalde':(newdate) => {
+
+    let Uvalderesult = UvaldeWaterLevel.findOne({
+      pdate: newdate
+    });
+     return Uvalderesult.MaxLevel;
+  }
+});
